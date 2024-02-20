b97af548a65f8d276c0bb6986154119507b2f6b9
Mateusz Antkiewicz <mateusz.gamewin1@gmail.com>
2024-02-20 23:00:06 +0100

    fix

---
+++ src/panel.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Panel({ twClassName }: { twClassName: string }) {
+    return(
+        <div className={twClassName}>
+
+        </div>
+    )
+}
+
+export default Panel;
