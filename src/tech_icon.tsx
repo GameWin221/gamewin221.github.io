1915e20daf6222da5d8e252e03799f14c7f3843d
Mateusz Antkiewicz <mateusz.gamewin1@gmail.com>
2024-02-20 22:18:39 +0100

    Updates

--- src/tech_icon.tsx
+++
@@ -1,7 +0,0 @@
-function TechIcon({href, src, className}: { href: string, src: string, className: string }) {
-    return(
-        <a href={href} className='hover:scale-125 transition ease-in-out duration-150'><img className={className} src={src} alt='tech_icon'></img></a>
-    )
-}
-
-export default TechIcon;
